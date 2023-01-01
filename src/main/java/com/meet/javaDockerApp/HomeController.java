970fe62927d5d6aab9100f4818f72854524ec24d
meetakbari <meetakbari.me@gmail.com>
2023-01-01 14:23:06 +0530

    final changes added

---
+++ src/main/java/com/meet/javaDockerApp/HomeController.java
@@ -0,0 +1,12 @@
+package com.meet.javaDockerApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello from first java docker app";
+    }
+}
